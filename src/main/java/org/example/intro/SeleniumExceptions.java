71e6e748f874d7c1095dcf3e8eb571ec7eddcbe2
Einar <toktogonoveinar@gmail.com>
2023-01-25 12:29:06 -0300

    Merging

--- src/main/java/org/example/intro/SeleniumExceptions.java
+++ src/main/java/org/example/intro/SeleniumExceptions.java
@@ -14,6 +14,7 @@ public class SeleniumExceptions extends BaseTest {
     @Test
     public void demo1() {
         //NoSuchElementException
+
         browserHelper.openURL("https://demoqa.com/text-box");
         driver.findElement(By.id("userName1")).sendKeys("Hello World");
     }
@@ -21,6 +22,7 @@ public class SeleniumExceptions extends BaseTest {
     @Test
     public void noSuchWindowException() {
         //noSuchWindowException
+
         browserHelper.openURL("https://demoqa.com/text-box");
         browserHelper.switchToWindow(3);
         driver.switchTo().window("hello");
@@ -72,6 +74,18 @@ public class SeleniumExceptions extends BaseTest {
     public void webDriverExceptionTest() {
         //WevDriverException
          browserHelper.openURL("https://www.amazon/test.page");
+    }
+    @Test
+    public void test1(){
+
+        browserHelper.openURL("https://demoqa.com/text-box");
+        WebElement link = driver.findElement(By.id("userName1"));
+        if(link.getAttribute("href") != null){
+            String linkUrl = link.getAttribute("href");
+            // do something with linkUrl
+        } else {
+            System.out.println("The link does not have an href attribute.");
+        }
     }
 
 }
