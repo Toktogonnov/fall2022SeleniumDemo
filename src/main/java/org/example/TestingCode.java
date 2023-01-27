736d6336023e076b3d943c08e6ff6ee99d37ae22
Einar <toktogonoveinar@gmail.com>
2023-01-27 11:33:21 -0300

    Coding test

--- src/main/java/org/example/TestingCode.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class TestingCode {
-    public static void main(String[] args) {
-        System.out.println();
-    }
-}
---
+++ src/main/java/org/example/master/Master.java
@@ -0,0 +1,4 @@
+package org.example.master;
+
+public class Master {
+}
